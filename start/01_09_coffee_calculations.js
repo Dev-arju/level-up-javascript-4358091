feeaac6853f610fe201661ec59c15f1fb899d625
Arjun Sajeevan <120627367+Dev-arju@users.noreply.github.com>
2023-08-01 08:44:05 +0000

    finished ninth

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,10 @@
 // Write your code here
+
+function calculateSum(...bill){
+  const totalCoffee = bill.reduce((accu, value)=>{
+    return accu += value
+  }, 0)
+  return `The total bill is ${totalCoffee * 1.25}`
+}
+
+console.log(calculateSum(2,3,1,5))

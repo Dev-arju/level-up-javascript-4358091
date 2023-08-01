03165d631b78ff5a73783ec399f2c90fef7a3825
Arjun Sajeevan <120627367+Dev-arju@users.noreply.github.com>
2023-08-01 09:30:52 +0000

    recrsion sample

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+function numPermutations(word){
+  if(word.length === 1){
+    return 1
+  }
+  
+  return word.length * numPermutations(word.slice(1))
+}
+
+console.log(numPermutations('hello'))

f25851e73508142a9ddd7e5870fd96830d33870d
Arjun Sajeevan <120627367+Dev-arju@users.noreply.github.com>
2023-08-01 09:52:35 +0000

    string manipulation

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,9 @@
 // Write your code here
+function urlfy(title){
+  const punctuation = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g;
+  const titleWithoutPunctuation = title.replace(punctuation, "")
+  console.log(titleWithoutPunctuation);
+  return titleWithoutPunctuation.toLowerCase().trim().replaceAll(" ", "-")
+}
+
+console.log(urlfy('I got a new Job !!!*'));

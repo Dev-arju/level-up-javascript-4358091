bd7aba4b4ba9e77cf2b428791f2b4c62f13fe244
Arjun Sajeevan <120627367+Dev-arju@users.noreply.github.com>
2023-08-01 08:01:39 +0000

    finished seventh

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,19 @@
 // Write your code here
+function getStudents(classroom){
+  const { hasTeachingAssistant, classlist } = classroom
+  let teacher, teachingAssistant, students;
+
+  if(hasTeachingAssistant){
+    [teacher, teachingAssistant, ...students] = classlist
+  }else{
+    [teacher, ...students] = classlist
+  }
+  return students
+}
+
+const classroom = {
+  classlist: ['Rashid', 'Ramu', 'Alan', 'Nihal', 'Athif'],
+  hasTeachingAssistant: true
+}
+
+console.log(getStudents(classroom));

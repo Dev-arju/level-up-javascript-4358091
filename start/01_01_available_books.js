e7070ba6b51508985ad40aa948d1e9213301aa72
Arjun Sajeevan <120627367+Dev-arju@users.noreply.github.com>
2023-08-01 07:07:58 +0000

    finished first & second

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,75 @@
 // Write your code here
+
+class Book {
+  constructor(title, author, isbn, numCopies) {
+    this.Title = title;
+    this.Author = author;
+    this.ISBN = isbn;
+    this.numCopies = numCopies;
+  }
+
+  get availability(){
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock";
+    } else {
+      return "In stock";
+    }
+  }
+
+  sell(numSold = 1) {
+    if (this.numCopies >= numSold) {
+      this.numCopies -= numSold;
+    } else {
+      return "Stock limit is reached";
+    }
+  }
+
+  reStock(numCopies = 5) {
+    this.numCopies += numCopies;
+  }
+}
+
+// function Book(title, author, isbn, numCopies){
+//   this.Title = title;
+//   this.Author = author;
+//   this.ISBN = isbn;
+//   this.numCopies = numCopies;
+// }
+
+// Book.prototype.getAvailability = function () {
+//   if (this.numCopies === 0) {
+//       return "Out of stock";
+//     } else if (this.numCopies < 10) {
+//       return "Low stock";
+//     } else {
+//       return "In stock";
+//     }
+// }
+
+// Book.prototype.sell = function (numSold = 1){
+//   if (this.numCopies >= numSold) {
+//     this.numCopies -= numSold;
+//   } else {
+//     return "Stock limit is reached";
+//   }
+// }
+
+// Book.prototype.reStock = function (numCopies = 5){
+//   this.numCopies += numCopies
+// }
+
+const hungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+
+console.log(hungerGames.availability);
+
+hungerGames.reStock(12)
+console.log(hungerGames.availability);
+
+hungerGames.sell(17)
+console.log(hungerGames.availability);
--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,41 @@
 // Write your code here
+
+// function Movie(title, director, genre, releaseYear, rating){
+//   this.Title = title;
+//   this.Director = director;
+//   this.Genre = genre;
+//   this.Release_year = releaseYear;
+//   this.Rating = rating
+// }
+
+// Movie.prototype.getOverview = function (){
+//   const { Title, Genre, Director, Release_year, Rating } = this
+//   return `${Title} a ${Genre} film directed by ${Director} was released
+//   in ${Release_year}. It recived a rating of ${Rating}`
+// }
+
+class Movie {
+  constructor(title, director, genre, releaseYear, rating){
+    this.Title = title;
+    this.Director = director;
+    this.Genre = genre;
+    this.Release_year = releaseYear;
+    this.Rating = rating
+  }
+
+  get overview(){
+    return this.getOverview();
+  }
+
+  getOverview(){
+    const { Title, Genre, Director, Release_year, Rating } = this
+    return `${Title} a ${Genre} film directed by ${Director} was released 
+    in ${Release_year}. It recived a rating of ${Rating}`
+  }
+}
+
+const spiderMan = new Movie('Spider Man', 'Sam Raimi', 'Action', 2002, 87);
+const batMan = new Movie('The Dark Night', 'Christopher Nolan', 'Action', 2008, 83)
+
+console.log(spiderMan);
+console.log(spiderMan.overview);
